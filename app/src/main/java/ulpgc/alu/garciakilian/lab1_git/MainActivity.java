4a8a078e1b574aa694f152f0499e4c9f0b092ad9
KIlian <kilian.garcia106@alu.ulpgc.es>
2020-02-12 11:05:19 +0000

    Documentacion inicial

--- app/src/main/java/ulpgc/alu/garciakilian/lab1_git/MainActivity.java
+++ app/src/main/java/ulpgc/alu/garciakilian/lab1_git/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Domentación inicial
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
